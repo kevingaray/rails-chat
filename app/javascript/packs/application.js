081bd98e0be40e3e5f2bcf49b36d28aaaaa4d085
kevin_gerardo <kevingerardo23@gmail.com>
2023-05-19 12:37:56 -0500

    enable dropdown functionality and create nav partial

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,15 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import '@doabit/semantic-ui-sass'
+import 'jquery'
+
+require("semantic-ui-sass")
+$(document).on('turbolinks:load', function() {
+  $('.ui.dropdown').dropdown();
+})
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-import '@doabit/semantic-ui-sass/src/scss/semantic-ui.scss'
